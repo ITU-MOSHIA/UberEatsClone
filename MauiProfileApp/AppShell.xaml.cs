5259fec140308bc038f27fbd1f8abef4a835b317
ITU-MOSHIA <moshiaitumeleng6@gmail.com>
2025-02-07 12:31:37 +0200

    itumeleng

---
+++ MauiProfileApp/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Maui.Controls;
+using MauiProfileApp.Views;
+
+namespace MauiProfileApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            RegisterRoutes();
+        }
+
+        private void RegisterRoutes()
+        {
+            Routing.RegisterRoute(nameof(ProfilePage), typeof(ProfilePage));
+        }
+    }
+}
---
+++ TaskConnect/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TaskConnect
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
